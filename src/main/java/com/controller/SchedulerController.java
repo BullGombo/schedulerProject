0d6c2159d079be8e156251a614f94cb1c6c452e6
BullGombo <ormaju15123647@gmail.com>
2025-11-05 17:54:05 +0900

    ✨ feat(all): 필수 기능 LV.0 : 파일 세팅
    프로퍼티, 빌드파일, 디렉터리 생성 등의 세팅만 해놓은 상태

---
+++ src/main/java/com/controller/SchedulerController.java
@@ -0,0 +1,25 @@
+package com.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class SchedulerController {
+
+    // 일정 생성
+    
+    
+    // 일정 단건 조회
+    
+    
+    // 일정 다건 조회
+    
+    
+    // 일정 수정
+    
+    
+    // 일정 삭제
+
+
+}
